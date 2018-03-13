6b6e3132c5d85d5fd8a60d3192047d6db8641167
Nicolasitse00 <motnic.ale.01@gmail.com>
2018-03-13 15:04:50 -0600

    se agrega formulario entradas

---
+++ trabajo1/entradasForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace trabajo1
+{
+    public partial class entradasForm : Form
+    {
+        public entradasForm()
+        {
+            InitializeComponent();
+        }
+
+        private void entradasBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.entradasBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.postgresDataSet);
+
+        }
+
+        private void entradasForm_Load(object sender, EventArgs e)
+        {
+            // TODO: esta línea de código carga datos en la tabla 'postgresDataSet.entradas' Puede moverla o quitarla según sea necesario.
+            this.entradasTableAdapter.Fill(this.postgresDataSet.entradas);
+
+        }
+    }
+}
